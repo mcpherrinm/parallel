95e6fe0b54c43fafcddd5c422bcfdf12505d2034
Matt McPherrin <git@mcpherrin.ca>
2014-07-04 02:02:10 -0700

    Make that a little less crap

--- src/parallel.rs
+++ src/parallel.rs
@@ -2,9 +2,7 @@ use std::mem::transmute;
 
 /// First try, definitely just a test and not the right API
 /// work is the closure to invoke on each chunk
-//pub fn parallel<'data, Data> (data: &'data mut [Data], parallelism: uint) {
-pub fn parallel<'data> (data: &'data mut [uint], parallelism: uint) {
-  // For differential testing, this is an entirely safe version that operates serially.
+pub fn parallel<'data, Data> (data: &'data mut [Data], parallelism: uint, work: fn(uint, &mut [Data])) {
   let mut chunk_size = data.len() / parallelism;
   if chunk_size*parallelism < data.len() { chunk_size += 1 }
   assert!(chunk_size*parallelism >= data.len());
@@ -14,17 +12,16 @@ pub fn parallel<'data> (data: &'data mut [uint], parallelism: uint) {
   for chunk in data.mut_chunks(chunk_size) {
     workeridx += 1;
     let thistx = tx.clone();
-    // We can't send a &mut [Data] directly.
-    let raw_chunk: std::raw::Slice<uint> = unsafe { transmute(chunk) };
+    // We can't send a &mut [Data] or *Data directly, so send the pointer and
+    // length as uints
+    let raw_chunk: std::raw::Slice<Data> = unsafe { transmute(chunk) };
     let datap: uint = unsafe { std::mem::transmute(raw_chunk.data)};
     let datalen = raw_chunk.len;
     spawn(proc(){
-      let raw_chunk = std::raw::Slice{data: unsafe { transmute::<uint,*const uint>(datap) }, len: datalen};
-      let unraw_chunk: &mut [uint] = unsafe { transmute(raw_chunk) };
-      println!("{} here!", workeridx);
-      for t in unraw_chunk.mut_iter() {
-        *t = workeridx;
-      }
+      let raw_chunk = std::raw::Slice{data: unsafe { transmute::<uint,*const uint>(datap) },
+                                      len: datalen};
+      let unraw_chunk: &mut [Data] = unsafe { transmute(raw_chunk) };
+      work(workeridx, unraw_chunk);
       thistx.send(workeridx);
     });
   }
@@ -40,8 +37,13 @@ pub fn parallel<'data> (data: &'data mut [uint], parallelism: uint) {
 #[test]
 fn test_parallel() {
   let mut data = [0u, ..1024];
+  fn foo(worker_id: uint, hunk: &mut [uint]) {
+    for t in hunk.mut_iter() {
+      *t = worker_id;
+    }
+  }
 
-  parallel(data.as_mut_slice(), 17);
+  parallel(data.as_mut_slice(), 17, foo);
 
   println!("data is {}", data.as_slice());
 
