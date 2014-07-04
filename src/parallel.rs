814061232df36b73e52801a63ac58184bbe5a03d
Matt McPherrin <git@mcpherrin.ca>
2014-07-04 01:52:38 -0700

    Overloaded Fn stuff was causing ICE

--- src/parallel.rs
+++ src/parallel.rs
@@ -1,26 +1,13 @@
-#![feature(overloaded_calls)]
 use std::mem::transmute;
 
-struct JobWorker<T> {
-  state: T
-}
-
-impl<'wut, T: Share+std::fmt::Show, Data:std::fmt::Show> std::ops::Fn<(&'wut mut [Data],), ()> for JobWorker<T> {
-  fn call(&self, (args,): (&mut [Data],)) {
-    println!("Worker here, workin'! {} on {}", self.state, args[0]);
-  }
-}
-
-/// First try, probalby not the right API
+/// First try, definitely just a test and not the right API
 /// work is the closure to invoke on each chunk
-pub fn parallel<'data, Data> (data: &'data mut [Data], parallelism: uint) {
+//pub fn parallel<'data, Data> (data: &'data mut [Data], parallelism: uint) {
+pub fn parallel<'data> (data: &'data mut [uint], parallelism: uint) {
   // For differential testing, this is an entirely safe version that operates serially.
   let mut chunk_size = data.len() / parallelism;
   if chunk_size*parallelism < data.len() { chunk_size += 1 }
   assert!(chunk_size*parallelism >= data.len());
-
-  // hax: this worker stuff needs to be passed in for this to be useful
-  let jobber = JobWorker{state: 42u};
 
   let mut workeridx = 0u;
   let (tx, rx) = std::comm::channel();
@@ -28,13 +15,16 @@ pub fn parallel<'data, Data> (data: &'data mut [Data], parallelism: uint) {
     workeridx += 1;
     let thistx = tx.clone();
     // We can't send a &mut [Data] directly.
-    let raw_chunk: std::raw::Slice<Data> = unsafe { transmute(chunk) };
+    let raw_chunk: std::raw::Slice<uint> = unsafe { transmute(chunk) };
     let datap: uint = unsafe { std::mem::transmute(raw_chunk.data)};
     let datalen = raw_chunk.len;
     spawn(proc(){
-      let raw_chunk = std::raw::Slice{data: unsafe { transmute::<uint,*const Data>(datap) }, len: datalen};
-      let unraw_chunk: &mut [Data] = unsafe { transmute(raw_chunk) };
-      jobber(unraw_chunk);
+      let raw_chunk = std::raw::Slice{data: unsafe { transmute::<uint,*const uint>(datap) }, len: datalen};
+      let unraw_chunk: &mut [uint] = unsafe { transmute(raw_chunk) };
+      println!("{} here!", workeridx);
+      for t in unraw_chunk.mut_iter() {
+        *t = workeridx;
+      }
       thistx.send(workeridx);
     });
   }
@@ -52,5 +42,7 @@ fn test_parallel() {
   let mut data = [0u, ..1024];
 
   parallel(data.as_mut_slice(), 17);
+
+  println!("data is {}", data.as_slice());
 
 }
